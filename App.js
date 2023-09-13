bc4371e43118a2564562baa0691a156422c356ae
BrendonLeeYiHao <brendonlee0413@gmail.com>
2023-09-13 22:54:16 +0800

    add header and todoitem

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import Header from './components/header';
+import TodoItem from './components/todoItem';
 
 export default function App() {
+
+  const [todos, setTodos] = useState([
+    { text: 'buy coffee', key: '1'},
+    { text: 'create an app', key: '2'},
+    { text: 'play on the switch', key: '3'}
+  ]);
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key != key);
+    })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Header/>
+      <View style={styles.content}>
+        {/* Form */}
+        <View style={styles.list}>
+          <FlatList data={todos} renderItem={({item}) => (
+            <TodoItem item={item} pressHandler={pressHandler}/>
+          )}/>
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +38,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+
+  content: {
+    padding:40
+  },
+
+  list:{
+    marginTop: 20
+  }
 });
